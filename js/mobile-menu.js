56549fc5d432ea014e7c50bf1f7bddce064bd5ee
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2022-12-12 22:43:23 +0100

    adds adaptive work foto

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,4 +1,4 @@
-/*(() => {
+(() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
@@ -22,4 +22,4 @@
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
-})(); */
+})(); 
